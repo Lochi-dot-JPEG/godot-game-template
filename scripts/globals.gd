958867fdd748201ce20c9932f38b19df4ab819fb
Lochie <95149116+Lochi-dot-JPEG@users.noreply.github.com>
2026-07-08 22:27:47 +1000

    initial commit
    basic scene, global variables, hud, license, gitignore

---
+++ scripts/globals.gd
@@ -0,0 +1,7 @@
+extends Node
+
+# Preload assets and store globally accessible variables here
+# This script is accessible anywhere in the project under the Globals autoload
+
+var high_score = 100
+var score = 0
